fbb49330f410c7fbb448d4c4f22f655f0456fdc6
Flora Hill <imflo.pink@gmail.com>
2025-05-29 00:11:31 +0000

    Allow inline media & force UA

--- App.tsx
+++ App.tsx
@@ -184,6 +184,8 @@ function Split({ config, logs, id }: { config: Config, logs: LogClient, id: stri
       style={{ flex: 1 }}
       source={{ uri: config.url }}
       injectedJavaScript={script}
+      allowsInlineMediaPlayback={true}
+      userAgent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
       onMessage={m => {
         let payload: { type: string, data: LogEntryData } | undefined;
         try {
